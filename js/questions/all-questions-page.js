062822917fd8ee5deb6d40a4810f4fa2426f844a
hadriensevel <hadrien.sevel@outlook.com>
2023-10-10 17:50:11 +0200

    Add questions feature flag check

--- js/questions/all-questions-page.js
+++ js/questions/all-questions-page.js
@@ -6,8 +6,9 @@ import {createElementFromTemplate} from './templates/utils';
 import {questionCardsWrapperTemplate} from './templates/question-cards-wrapper';
 import {loadQuestionCards} from './handle-question-card';
 import {QuestionLocation} from './utils';
+import {getFeatureFlag} from '../utils/feature-flags';
 
-function allQuestions() {
+async function allQuestions() {
   // Create wrapper for the questions
   const questionsDiv = document.createElement('div');
   questionsDiv.id = 'all-questions';
@@ -17,6 +18,13 @@ function allQuestions() {
   // Select the first script tag and insert the questions div before it
   const scriptTag = document.body.querySelector('script');
   scriptTag.before(questionsDiv);
+
+  // Check the feature flag for the questions
+  const questionsFeatureFlag = await getFeatureFlag('questions');
+  if (questionsFeatureFlag === false) {
+    questionsDiv.innerHTML = '<p>Les questions ne sont pas disponibles pour le moment.</p>';
+    return;
+  }
 
   // Get the id of the question if it is in the URL of the parent page and remove it
   const urlParams = new URLSearchParams(window.parent.location.search);
