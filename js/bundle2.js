ad48098bb135d5e16ea98a4175777afd7774bf23
hadriensevel <hadrien.sevel@outlook.com>
2023-12-27 12:16:32 +0100

    Change jsxgraph version to fix katex

--- js/bundle2.js
+++ js/bundle2.js
@@ -1,5 +1,6 @@
 // Import JSXGraph
-import '../node_modules/jsxgraph/distrib/jsxgraphcore.js';
+import './distrib/jsxgraphcore.js';
+JXG.Options.text.useKatex = true;
 
 // Import Plyr
 import '../node_modules/plyr/dist/plyr.min.js';
