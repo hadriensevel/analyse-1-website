11c92e65a471a69a98a1342a5fb189101471fdc1
Mw3y <maxence1604.minecraft@gmail.com>
2024-09-06 16:27:41 +0200

    Only enable code highlighting for code blocks with a specified language

--- js/utils/code-highlighting.js
+++ js/utils/code-highlighting.js
@@ -1,14 +1,17 @@
-import {codeToHtml} from 'shiki';
+import { codeToHtml } from "shiki";
 
 async function highlightCode() {
-    const codeBlocks = document.querySelectorAll('code');
-    for (const block of codeBlocks) {
-        const lang = block.getAttribute('data-lang');
-        block.innerHTML = await codeToHtml(block.textContent, {
-            lang: lang,
-            theme: 'github-light-default',
-        });
-    }
+	const codeBlocks = document.querySelectorAll("code");
+	for (const block of codeBlocks) {
+		const lang = block.getAttribute("data-lang");
+		// Enable highlighting only for code blocks with a language
+		if (lang !== null) {
+			block.innerHTML = await codeToHtml(block.textContent, {
+				lang: lang,
+				theme: "github-light-default",
+			});
+		}
+	}
 }
 
-export {highlightCode};
+export { highlightCode };
